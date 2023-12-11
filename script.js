3a392e8fc5d01db875c0a05d4f6399f1e6680f14
mariusmoldeklev <mariusm.koder@gmail.com>
2023-12-11 14:48:22 +0100

    css and script update

--- script.js
+++ script.js
@@ -29,3 +29,90 @@ menuToggle.onclick = function(){
         // } else {
         //     lightToggle.src = 'icons/lightModeToggleButton.png';
         // }
+
+          //   document.addEventListener('DOMContentLoaded', function () {
+          //   const sideIconsContainer = document.getElementById('side-icons');
+          
+          //   // Define the paths for each icon
+          //   const iconPaths = [
+          //     'assets/icons/fotball.png',
+          //     'assets/icons/handball.png',
+          //     'assets/icons/karate.png',
+          //     'assets/icons/turOGkano.png',
+          //     'assets/icons/idretsskole.png',
+          //     'assets/icons/orientering.png',
+          //     'assets/icons/ski.png',
+          //     'assets/icons/åpen hall.png',
+          //   ];
+            
+            
+          //   // Add icons dynamically
+          //   for (let i = 0; i < 8; i++) {
+          //     const sideIcon = document.createElement('div');
+          //     sideIcon.className = 'side-icon';
+          
+          //     // Create an img element with the appropriate src attribute
+          //     const imgElement = document.createElement('img');
+          //     imgElement.src = iconPaths[i];
+          //     imgElement.alt = `Icon ${i + 1}`;
+              
+          //     sideIcon.appendChild(imgElement);
+          //     sideIconsContainer.appendChild(sideIcon);
+          //   }
+          
+          //   // Handle scroll event
+          //   window.addEventListener('scroll', function () {
+          //     const scrollPosition = window.scrollY;
+          
+          //     // You can adjust the scroll position threshold as needed
+          //     if (scrollPosition > 100) {
+          //       sideIconsContainer.style.opacity = 1;
+          //     } else {
+          //       sideIconsContainer.style.opacity = 0;
+          //     }
+          //   });
+          // });
+
+          document.addEventListener('DOMContentLoaded', function () {
+            const sideIconsContainer = document.getElementById('side-icons');
+          
+            // Define the paths for each icon
+            const iconPaths = [
+              'assets/icons/fotball.png',
+              'assets/icons/handball.png',
+              'assets/icons/karate.png',
+              'assets/icons/turOGkano.png',
+              'assets/icons/idretsskole.png',
+              'assets/icons/orientering.png',
+              'assets/icons/ski.png',
+              'assets/icons/åpen hall.png',
+            ];
+          
+            // Add icons dynamically
+            for (let i = 0; i < 8; i++) {
+              const sideIcon = document.createElement('div');
+              sideIcon.className = 'side-icon';
+          
+              // Create an img element with the appropriate src attribute
+              const imgElement = document.createElement('img');
+              imgElement.src = iconPaths[i];
+              imgElement.alt = `Icon ${i + 1}`;
+          
+              sideIcon.appendChild(imgElement);
+              sideIconsContainer.appendChild(sideIcon);
+            }
+          
+            // Handle scroll event
+            window.addEventListener('scroll', function () {
+              const scrollPosition = window.scrollY;
+          
+              // You can adjust the scroll position threshold as needed
+              if (scrollPosition > 100) {
+                sideIconsContainer.style.opacity = 1;
+              } else {
+                sideIconsContainer.style.opacity = 0;
+              }
+            });
+          });
+          
+          
